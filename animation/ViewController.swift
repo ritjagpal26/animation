b59c9abd6b137991ae54d772d95660b2232ac51c
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2020-01-07 20:27:48 -0500

    tictac

--- animation/ViewController.swift
+++ animation/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var activePlaye = 1 // Cross
+    
+    var gamestate = [0,0,0,0,0,0,0,0,0]
+    
+    @IBAction func action(_ sender: AnyObject)
+    {
+        if (gamestate[sender.tag-1] == 0 )
+        {
+            gamestate[sender.tag - 1] = activePlaye
+            if (activePlaye == 1)
+            {
+                sender.setImage(UIImage(named: "download (1).png"), for: UIControl.State())
+                activePlaye = 2
+            }
+            else
+            {
+                sender.setImage(UIImage(named: "download (2).png"), for: UIControl.State())
+                activePlaye = 1
+            }
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
